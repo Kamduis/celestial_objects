1cf648e16a17ee3b4722d3bf879f59297380c27e
Victor <victor@caern.de>
2024-11-22 19:00:36 +0100

    FIxed tests: Missing `mass` field.

--- src/tests/systems_examples.rs
+++ src/tests/systems_examples.rs
@@ -41,6 +41,7 @@ pub(crate) fn systems_example() -> Vec<CelestialSystem> {
 							name: Some( "Mercury".to_string() ),
 							radius: 0.3829,
 							gravity: 0.38,
+							mass: None,
 							rotation_period: Some( TimeDelta::days( 58 ) + TimeDelta::hours( 15 ) + TimeDelta::minutes( 36 ) ),
 							temperature: [ -173.0, 167.0, 427.0 ],
 							atmosphere: None,
@@ -56,6 +57,7 @@ pub(crate) fn systems_example() -> Vec<CelestialSystem> {
 							name: Some( "Venus".to_string() ),
 							radius: 0.9499,
 							gravity: 0.904,
+							mass: None,
 							rotation_period: Some( ( TimeDelta::days( 243 ) + TimeDelta::minutes( 36 ) ) * -1 ),
 							temperature: [ 437.0, 464.0, 497.0 ],
 							atmosphere: Some( Atmosphere {
@@ -80,6 +82,7 @@ pub(crate) fn systems_example() -> Vec<CelestialSystem> {
 							name: Some( "Terra".to_string() ),
 							radius: 1.0,
 							gravity: 1.0,
+							mass: None,
 							rotation_period: Some( TimeDelta::hours( 23 ) + TimeDelta::minutes( 56 ) + TimeDelta::seconds( 4 ) ),
 							temperature: [ -89.0, 15.0, 58.0 ],
 							atmosphere: Some( Atmosphere {
@@ -103,6 +106,7 @@ pub(crate) fn systems_example() -> Vec<CelestialSystem> {
 										name: Some( "Luna".to_string() ),
 										radius: 0.2731,
 										gravity: 0.1654,
+										mass: None,
 										rotation_period: None,
 										temperature: [ -173.0, -23.0, 116.0 ],
 										atmosphere: None,
@@ -145,6 +149,7 @@ pub(crate) fn systems_example() -> Vec<CelestialSystem> {
 							name: Some( "Saturn".to_string() ),
 							radius: 9.449,
 							gravity: 1.065,
+							mass: None,
 							rotation_period: Some( TimeDelta::hours( 10 ) + TimeDelta::minutes( 33 ) ),
 							temperature: [ -185.0, -152.0, -120.0 ],
 							atmosphere: Some( Atmosphere {
@@ -177,6 +182,7 @@ pub(crate) fn systems_example() -> Vec<CelestialSystem> {
 							name: Some( "Neptun".to_string() ),
 							radius: 3.829,
 							gravity: 1.137,
+										mass: None,
 							rotation_period: Some( TimeDelta::hours( 15 ) + TimeDelta::minutes( 57 ) + TimeDelta::seconds( 59 ) ),
 							temperature: [ -218.0, -209.0, -201.0 ],
 							atmosphere: Some( Atmosphere {
@@ -198,6 +204,7 @@ pub(crate) fn systems_example() -> Vec<CelestialSystem> {
 										name: Some( "Triton".to_string() ),
 										radius: 0.2122,
 										gravity: 0.0794,
+										mass: None,
 										rotation_period: None,
 										temperature: [ -235.0, -235.0, -235.0 ],
 										atmosphere: None,
@@ -232,6 +239,7 @@ pub(crate) fn systems_example() -> Vec<CelestialSystem> {
 										name: Some( "Minos".to_string() ),
 										radius: 1.1,
 										gravity: 1.05,
+										mass: None,
 										rotation_period: Some( TimeDelta::hours( 18 ) + TimeDelta::minutes( 7 ) ),
 										temperature: [ -7.0, 12.0, 89.0 ],
 										atmosphere: Some( Atmosphere {
@@ -265,6 +273,7 @@ pub(crate) fn systems_example() -> Vec<CelestialSystem> {
 										name: Some( "Taurus".to_string() ),
 										radius: 0.9,
 										gravity: 1.1,
+										mass: None,
 										rotation_period: Some( TimeDelta::hours( 30 ) + TimeDelta::minutes( 42 ) ),
 										temperature: [ -120.0, 5.0, 127.0 ],
 										atmosphere: Some( Atmosphere {
